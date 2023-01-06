3582fe0b64803f938eecfa926f94ac116513a0e9
nikolayShvetsov <76684191+nikolayShvetsov@users.noreply.github.com>
2023-01-06 16:54:48 +0300

    update and rename in twoForAndTwoCounts.cs

--- twoForAndTwoCounts.cs
+++ twoForAndTwoCounts.cs
@@ -14,6 +14,11 @@ namespace Lesson
             for (; i < 6; i++) {
                 Console.WriteLine("for_2: " + i);
             }
+            
+            for (int i = 0, k = 6; i < 3 && k < 12; i++, k++) {
+                Console.WriteLine("i: " + i);
+                Console.WriteLine("k: " + k);
+            }
 
             Console.ReadLine();
             
